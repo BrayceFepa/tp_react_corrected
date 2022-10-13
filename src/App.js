048f10e2e86b13538075e9df0ee8f0cb429eea2f
Brayce <braycefepa@gmail.com>
2022-10-13 15:45:42 +0100

    incrementqty and decrement qty

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-
     const prod = cart.filter((item) => {
       return (item.id === product.id);
     })[0];
+
 
     if (!prod) {
       product["quantity"] = 1;
@@ -39,24 +39,29 @@ function App() {
 
   // const [quantity, setQuantiy] = useState(2);
   const incrementQuantity = (product) => {
-    const index = cart.indexOf(product);
-    const cart2 = cart;
-    cart2[index].quantity += 1;
-    // console.log("cart2 no spread", cart2);
-    // console.log("with spread", [...cart2]);
-    setCart([...cart2]);
+    const updatedList = cart.map((item) => {
+
+        if (item.id === product.id) {
+          return {...item, quantity: item.quantity += 1}
+        }
+        return item
+
+      })
+
+      setCart(updatedList)
   };
 
   const decrementQuantity = (product) => {
-    const index = cart.indexOf(product);
-    const cart3 = cart;
-    cart3[index].quantity -= 1;
+    const updatedList = cart.map((item) => {
 
-    if (cart3[index].quantity === 0) {
-      cart3[index].quantity = 1;
-    }
+        if (item.id === product.id) {
+          return {...item, quantity: item.quantity -= 1}
+        }
+        return item
 
-    setCart([...cart3]);
+      })
+
+      setCart(updatedList)
   };
 
   return (
