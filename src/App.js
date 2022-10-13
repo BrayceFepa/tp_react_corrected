7b1d96a39dc989baf916ffa993e69098f501e055
Brayce <braycefepa@gmail.com>
2022-10-13 15:03:19 +0100

    use of filter and useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import './App.css';
-import Header from './Components/Header';
-import Hero from './Components/Hero';
-import CountDown from './Components/CountDown';
-import Footer from './Components/Footer';
-import Main from './Components/Main';
-import { useState } from 'react';
+import "./App.css";
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import CountDown from "./Components/CountDown";
+import Footer from "./Components/Footer";
+import Main from "./Components/Main";
+import { useState } from "react";
 
 function App() {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    product["quantity"] = 1;
-  if (cart.indexOf(product) === -1) {
-    setCart([...cart, product]);
-  } 
+
+    const prod = cart.filter((item) => {
+      return (item.id === product.id);
+    })[0];
+
+    if (!prod) {
+      product["quantity"] = 1;
+      setCart([...cart, product]);
+    } else {
+
+      const updatedList = cart.map((item) => {
+
+        if (item.id === product.id) {
+          return {...item, quantity: item.quantity += 1}
+        }
+        return item
+
+      })
+
+      setCart(updatedList)
+    }
+      
+
     
-    console.log("product", cart);
-  }
-  
+    
+  };
+
   // const [quantity, setQuantiy] = useState(2);
   const incrementQuantity = (product) => {
     const index = cart.indexOf(product);
@@ -26,27 +45,32 @@ function App() {
     // console.log("cart2 no spread", cart2);
     // console.log("with spread", [...cart2]);
     setCart([...cart2]);
-  }
+  };
 
   const decrementQuantity = (product) => {
     const index = cart.indexOf(product);
     const cart3 = cart;
     cart3[index].quantity -= 1;
-    
-    if (cart3[index].quantity === 0) { 
-        cart3[index].quantity = 1
+
+    if (cart3[index].quantity === 0) {
+      cart3[index].quantity = 1;
     }
-    
+
     setCart([...cart3]);
-  }
+  };
 
   return (
     <div className="App">
-      <Header length={cart.length} cart={cart} incrementQuantity={incrementQuantity} decrementQuantity={decrementQuantity} />
+      <Header
+        length={cart.length}
+        cart={cart}
+        incrementQuantity={incrementQuantity}
+        decrementQuantity={decrementQuantity}
+      />
       <Hero />
-      <Main addToCart={addToCart}/>
+      <Main addToCart={addToCart} />
       <CountDown />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
